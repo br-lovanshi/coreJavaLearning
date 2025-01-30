5af3b99e88a6820cf3bee8d1df4ddb31269acee2
Brajesh Lovanshi <brajesh.lovanshi@lendingkart.com>
2025-01-30 16:45:12 +0530

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(Arrays.toString(args));
+        System.out.println(args[1]);
     }
 }
